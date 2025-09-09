3cb71dab58ad3d1c3fa6c200fbba035eb29da4c1
JunheeLee <80439077+junheelee411@users.noreply.github.com>
2025-09-09 17:52:31 +0900

    HW

---
+++ Quiz05.java
@@ -0,0 +1,22 @@
+package HW;
+
+import java.util.Scanner;
+
+public class Quiz05 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		double distance,velocity, sec;
+		int hour, min;
+		
+		
+		
+		System.out.printf("");
+		
+		sc.close();
+
+	}
+
+}
