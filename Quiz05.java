ccebe6d2db50f2c7045f1265fd9507974e4d64b1
JunheeLee <80439077+junheelee411@users.noreply.github.com>
2025-09-10 10:02:36 +0900

    Update Quiz05.java

--- Quiz05.java
+++ Quiz05.java
@@ -1,4 +1,5 @@
-package academy_HW;
+
+package HW;
 
 import java.util.Scanner;
 
@@ -8,7 +9,9 @@ public class Quiz05 {
 		
 		Scanner sc = new Scanner(System.in);
 		
-		double distance,velocity,time,hour,min,sec;
+		double distance,velocity;
+		double time,hour,min,sec;
+		
 		//주행거리 입력
 		System.out.println("주행 거리(km) ?");
 		distance = sc.nextInt();
@@ -16,14 +19,14 @@ public class Quiz05 {
 		System.out.println("시속(km/h) ?");
 		velocity = sc.nextInt();
 		
-		time=(distance/velocity);
+		time=(double)(distance/velocity);
+		hour=(int)time;
+		min=(int)(time*60%60);
+		sec=(time*3600)%60;
+		sec=(int)(sec*100)/100.0;
 		
-		hour=(distance/velocity);
-		min=(distance%velocity/60);
-		sec=distance%3600;
 		
-		
-		System.out.printf("%.0f시간 %.0f분 %.3f초 소요",hour,min,sec);
+		System.out.printf("%.0f시간 %.0f분 %.2f초 소요",hour,min,sec);
 		
 		sc.close();
 
